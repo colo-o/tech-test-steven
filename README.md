ea4080292f4019c4dfad0c8fab4bc2101ebcb6db
Fernando Pintado <ferpintado@gmail.com>
2022-02-24 12:53:53 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,12 +1,62 @@
-# Getting Started with Create React App
+# Dooper - Technical Test
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Hola!
 
-## Available Scripts
+Thank you for your interest in joining our team and taking the time to complete this technical test.
+Once completed, please reply to our email with a link to a Github repository. Make sure the application builds and works successfully.
 
-In the project directory, you can run:
+## Challenge
 
-### `npm start`
+You will be creating a Movie Awards Ballot with the following requirements:
+
+- Display a list of movies grouped by categories ordered alphabetically
+- Search for a movie title
+- Select a movie per category and show a "selected" state
+- Upon submission, display a modal box showing your votes
+- When the modal is closed, the ballot resets and you can vote again.
+
+### Mockups
+
+![Wireframe](src/mockup.png?raw=true 'Wireframe')
+
+### Evaluation
+
+1.- Knowledge of component modularization (code reusability, best practices, folder structure)
+
+2.- React Hooks
+
+3.- Use of state management (ie. context, react-redux, etc)
+
+4.- Use of API requests (http://localhost:8080/api/movies)
+
+5.- Responsiveness (mobile-friendly)
+
+6.- User friendly
+
+**Bonus points:**
+
+- Use of Typescript
+- Make it look pretty (styling), show off your creative side!
+- Add Unit testing
+
+Feel free to use any third-party libraries that may help you accomplish the challenge.
+
+### Instructions
+
+Clone our repository
+
+Make sure you have installed
+
+- Node
+- NPM / Yarn
+
+Run:
+
+### `yarn install`
+
+Install dependencies
+
+### `yarn start`
 
 Runs the app in the development mode.\
 Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
@@ -14,12 +64,16 @@ Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 The page will reload if you make edits.\
 You will also see any lint errors in the console.
 
-### `npm test`
+### `yarn api`
+
+Runs the server on http://localhost:8080/ with the endpoint http://localhost:8080/api/movies which returns the list of movies
+
+### `yarn test`
 
 Launches the test runner in the interactive watch mode.\
 See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
 
-### `npm run build`
+### `yarn build`
 
 Builds the app for production to the `build` folder.\
 It correctly bundles React in production mode and optimizes the build for the best performance.
@@ -28,19 +82,3 @@ The build is minified and the filenames include the hashes.\
 Your app is ready to be deployed!
 
 See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
---
+++ api/index.js
@@ -0,0 +1,259 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 8080;
+app.listen(port);
+
+const generateMoviesData = () => {
+  return {
+    movies: [
+      {
+        title: 'Nomadland',
+        photoUrL:
+          'https://variety.com/wp-content/uploads/2020/12/nomadland_ver2.jpg',
+        id: 'nomadland',
+        category: 'Best Picture',
+      },
+      {
+        title: 'The Trial of the Chicago 7',
+        photoUrL:
+          'https://variety.com/wp-content/uploads/2020/12/trial_of_the_chicago_seven.jpg',
+        id: 'the-trial-of-the-chicago-7',
+        category: 'Best Picture',
+      },
+      {
+        title: 'Minari',
+        photoUrL: 'https://variety.com/wp-content/uploads/2020/12/Minari.jpg',
+        id: 'minari',
+        category: 'Best Picture',
+      },
+      {
+        title: 'Mank',
+        photoUrL: 'https://variety.com/wp-content/uploads/2020/12/mank.jpg',
+        id: 'mank',
+        category: 'Best Picture',
+      },
+      {
+        title: 'One Night in Miami',
+        photoUrL:
+          'https://variety.com/wp-content/uploads/2020/12/one_night_in_miami.jpg',
+        id: 'one-night-in-miami',
+        category: 'Best Picture',
+      },
+      {
+        title: 'Chloé Zhao for Nomadland',
+        photoUrL:
+          'https://variety.com/wp-content/uploads/2020/12/nomadland_ver2.jpg',
+        id: 'chloe-zhao',
+        category: 'Best Director',
+      },
+      {
+        title: 'Aaron Sorkin for The Trial of the Chicago 7',
+        photoUrL:
+          'https://variety.com/wp-content/uploads/2020/12/trial_of_the_chicago_seven.jpg',
+        id: 'aaron-sorkin',
+        category: 'Best Director',
+      },
+      {
+        title: 'Lee Isaac Chung for Minari',
+        photoUrL: 'https://variety.com/wp-content/uploads/2020/12/Minari.jpg',
+        id: 'lee-isaac-chung',
+        category: 'Best Director',
+      },
+      {
+        title: 'David Fincher for Mank',
+        photoUrL: 'https://variety.com/wp-content/uploads/2020/12/mank.jpg',
+        id: 'david-fincher',
+        category: 'Best Director',
+      },
+      {
+        title: 'Regina King for One Night in Miami',
+        photoUrL:
+          'https://variety.com/wp-content/uploads/2020/12/one_night_in_miami.jpg',
+        id: 'regina-king',
+        category: 'Best Director',
+      },
+      {
+        title: "Chadwick Boseman for Ma Rainey's Black Bottom",
+        photoUrL:
+          'https://variety.com/wp-content/uploads/2020/12/ma_raineys_black_bottom_ver2.jpg',
+        id: 'chadwick-boseman',
+        category: 'Best Actor',
+      },
+      {
+        title: 'Anthony Hopkins for The Father',
+        photoUrL:
+          'https://variety.com/wp-content/uploads/2020/12/father_ver3.jpg',
+        id: 'anthony-hopkins',
+        category: 'Best Actor',
+      },
+      {
+        title: 'Delroy Lindo for Da 5 Bloods',
+        photoUrL:
+          'https://variety.com/wp-content/uploads/2020/12/da_five_bloods_ver2.jpg',
+        id: 'delroy-lindo',
+        category: 'Best Actor',
+      },
+      {
+        title: 'Steven Yeun for Minari',
+        photoUrL: 'https://variety.com/wp-content/uploads/2020/12/minari-1.jpg',
+        id: 'steven-yeun',
+        category: 'Best Actor',
+      },
+      {
+        title: 'Riz Ahmed for The Sound of Metal',
+        photoUrL:
+          'https://variety.com/wp-content/uploads/2020/12/sound_of_metal_ver2.jpg',
+        id: 'riz-ahmed',
+        category: 'Best Actor',
+      },
+      {
+        title: 'Vanessa Kirby for Pieces of a Woman',
+        photoUrL:
+          'https://variety.com/wp-content/uploads/2020/12/pieces_of_a_woman.jpg',
+        id: 'vanessa-kirby',
+        category: 'Best Actress',
+      },
+      {
+        title: 'Frances McDormand for Nomadland',
+        photoUrL:
+          'https://variety.com/wp-content/uploads/2020/12/nomadland_ver2.jpg',
+        id: 'frances-mcdormand',
+        category: 'Best Actress',
+      },
+      {
+        title: 'Carey Mulligan for Promising Young Woman',
+        photoUrL:
+          'https://variety.com/wp-content/uploads/2020/12/promising_young_woman_ver2.jpg',
+        id: 'carey-mulligan',
+        category: 'Best Actress',
+      },
+      {
+        title: "Viola Davis for Ma Rainey's Black Bottom",
+        photoUrL:
+          'https://variety.com/wp-content/uploads/2020/12/ma_raineys_black_bottom.jpg',
+        id: 'viola-davis',
+        category: 'Best Actress',
+      },
+      {
+        title: 'Meryl Streep for The Prom',
+        photoUrL:
+          'https://variety.com/wp-content/uploads/2020/12/prom_ver2.jpg',
+        id: 'meryl-streep',
+        category: 'Best Actress',
+      },
+
+      {
+        title: 'Daniel Kaluuya for Judas and the Black Messiah',
+        photoUrL:
+          'https://variety.com/wp-content/uploads/2020/12/judas_and_the_black_messiah.jpg',
+        id: 'daniel-kaluuya',
+        category: 'Best Supporting Actor',
+      },
+      {
+        title: 'Leslie Odom, Jr. for One Night in Miami',
+        photoUrL:
+          'https://variety.com/wp-content/uploads/2020/12/one_night_in_miami.jpg',
+        id: 'leslie-odom',
+        category: 'Best Supporting Actor',
+      },
+      {
+        title: 'Sacha Baron Cohen for The Trial of the Chicago 7',
+        photoUrL:
+          'https://variety.com/wp-content/uploads/2020/12/trial_of_the_chicago_seven.jpg',
+        id: 'sacha-baron-cohen',
+        category: 'Best Supporting Actor',
+      },
+      {
+        title: 'Stanley Tucci for Supernova',
+        photoUrL:
+          'https://variety.com/wp-content/uploads/2020/12/supernova.jpg',
+        id: 'stanley-tucci',
+        category: 'Best Supporting Actor',
+      },
+      {
+        title: 'Chadwick Boseman for Da 5 Bloods',
+        photoUrL:
+          'https://variety.com/wp-content/uploads/2020/12/da_five_bloods_ver2.jpg',
+        id: 'chadwick-boseman',
+        category: 'Best Supporting Actor',
+      },
+      {
+        title: 'Olivia Colman for The Father',
+        photoUrL:
+          'https://variety.com/wp-content/uploads/2020/12/father_ver3.jpg',
+        id: 'olivia-coleman',
+        category: 'Best Supporting Actress',
+      },
+      {
+        title: 'Amanda Seyfried for Mank',
+        photoUrL: 'https://variety.com/wp-content/uploads/2020/12/mank.jpg',
+        id: 'amanda-seyfried',
+        category: 'Best Supporting Actress',
+      },
+      {
+        title: 'Ellen Burstyn for Pieces of a Woman',
+        photoUrL:
+          'https://variety.com/wp-content/uploads/2020/12/pieces_of_a_woman.jpg',
+        id: 'ellen-burstyn',
+        category: 'Best Supporting Actress',
+      },
+      {
+        title: 'Maria Bakalova for Borat Subsequent Moviefilm',
+        photoUrL:
+          'https://variety.com/wp-content/uploads/2020/12/borat_two_ver2.jpg',
+        id: 'maria-bakalova',
+        category: 'Best Supporting Actress',
+      },
+      {
+        title: 'Yuh-jung Youn for Minari',
+        photoUrL: 'https://variety.com/wp-content/uploads/2020/12/minari-1.jpg',
+        id: 'yuh-jung-youn',
+        category: 'Best Supporting Actress',
+      },
+      {
+        title: 'The Midnight Sky',
+        photoUrL:
+          'https://variety.com/wp-content/uploads/2020/12/midnight_sky_ver2.jpg',
+        id: 'midnight-sky',
+        category: 'Best Visual Effects',
+      },
+      {
+        title: 'Tenet',
+        photoUrL:
+          'https://variety.com/wp-content/uploads/2020/12/tenet_ver7.jpg',
+        id: 'tenet',
+        category: 'Best Visual Effects',
+      },
+      {
+        title: 'Wonder Woman 1984',
+        photoUrL:
+          'https://variety.com/wp-content/uploads/2020/12/wonder_woman_nineteen_eighty_four_ver18.jpg',
+        id: 'wonder-woman',
+        category: 'Best Visual Effects',
+      },
+      {
+        title: 'The Invisible Man',
+        photoUrL:
+          'https://variety.com/wp-content/uploads/2020/12/invisible_man.jpg',
+        id: 'the-invisible-man',
+        category: 'Best Visual Effects',
+      },
+      {
+        title: 'Mulan',
+        photoUrL: 'https://variety.com/wp-content/uploads/2020/12/minari-1.jpg',
+        id: 'mulan',
+        category: 'Best Visual Effects',
+      },
+    ],
+  };
+};
+
+const movies = generateMoviesData();
+
+app.get('/api/movies', (req, res) => {
+  res.json(movies);
+});
+
+console.log('App is listening on port ' + port);
